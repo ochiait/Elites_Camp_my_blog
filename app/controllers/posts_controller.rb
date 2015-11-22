27549dc4547e222e165fb340c08ecfb8af9a1ea2
Tetsuya Ochiai <ochiai@ryorika-mm.jp>
2015-11-22 15:31:54 +0900

    posts_controller内メソッド作成（index,show,new,create）

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,17 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(item_params)
+     # 上の行はインスタンスを作っただけ。saveメソッドで保存しないと、消える。
+    @post.save
+    redirect_to "/posts/#{@post.id}"
+  end
 end
