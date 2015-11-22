d27dfaada6393ed1b45d36d84233be2b04dbe2b2
Tetsuya Ochiai <ochiai@ryorika-mm.jp>
2015-11-22 15:38:05 +0900

    posts_controller内メソッド修正(create,edit)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,12 +6,19 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
   def new
-    @post = Post.new
-  end
-  def create
-    @post = Post.new(item_params)
-     # 上の行はインスタンスを作っただけ。saveメソッドで保存しないと、消える。
-    @post.save
-    redirect_to "/posts/#{@post.id}"
-  end
+     @post = Post.new
+   end
+   def create
+     @post = Post.new(post_params)
+     @post.save
+     redirect_to "/posts/#{@post.id}"
+   end
+
+   private
+     def post_params
+       params.require(:post).permit(
+         :title,
+         :category,
+         :body)
+     end
 end
