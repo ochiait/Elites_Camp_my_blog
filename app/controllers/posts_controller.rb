4c30d558bb15de8847da46673eb5e1a74bde5d87
Tetsuya Ochiai <ochiai@ryorika-mm.jp>
2015-11-22 16:16:40 +0900

    Postコントローラー修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,37 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @author = Author.first
   end
   def show
     @post = Post.find(params[:id])
   end
   def new
-     @post = Post.new
-   end
-   def create
-     @post = Post.new(post_params)
-     @post.save
-     redirect_to "/posts/#{@post.id}"
-   end
-
-   private
-     def post_params
-       params.require(:post).permit(
-         :title,
-         :category,
-         :body)
-     end
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to "/posts/#{@post.id}"
+  end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to "/posts/#{@post.id}"
+  end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to "/"
+  end
+private
+  def post_params
+     params.require(:post).permit(
+        :title,
+        :category,
+        :body)
+  end
 end
